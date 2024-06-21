1500f68342967f4674542a4a7335a1f55bd442b7
Marcin Stankiewicz <marcinstankiewicz222@gmail.com>
2024-06-21 08:12:25 +0200

    Update embedding-service.py

--- embedding-server/old_gpu_pytorch/embedding-service.py
+++ embedding-server/old_gpu_pytorch/embedding-service.py
@@ -67,4 +67,4 @@ async def get_embeddings(item: Item):
     return response
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, access_log=True)
+    uvicorn.run(app, host="0.0.0.0", port=9200, access_log=True)
